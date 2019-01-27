ad247dddd27492fa7ec693cd9f70a6de866dad18
qetennyson <quincytennyson8@gmail.com>
2019-01-27 16:32:07 -0500

    basic exception handling, no specificity

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ def search_event_loop():
     search = 'ONCE_THROUGH_LOOP'
 
     while search != 'x':
-        search = input("Movie search text (x to exit): ")
-        if search != 'x':
-            results = movie_svc.find_movies(search)
-            print(f'Found {len(results)} results.')
-            for r in results:
-                print(f'{r.year} -- {r.title}')
-            print()
+        try:
+            search = input("Movie search text (x to exit): ")
+            if search != 'x':
+                results = movie_svc.find_movies(search)
+                print(f'Found {len(results)} results.')
+                for r in results:
+                    print(f'{r.year} -- {r.title}')
+                print()
+        #TODO: Exception should handle individual errors.  Not simply say it didn't work for any reason.
+        # Try removing Internet connectivity, OR searching for nothing.
+        except:
+            print("Whoops, that didn't work.")
 
     print("exiting... ")
 
